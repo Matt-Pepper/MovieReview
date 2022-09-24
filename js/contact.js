b31976b188bddbd79db75b2ea0277b1ad8a97789
Matt-Pepper <matthew.g.pepper@gmail.com>
2022-09-24 18:56:45 +1000

    formating

--- js/contact.js
+++ js/contact.js
@@ -1,18 +1,18 @@
 function clearForm() {
-  document.getElementById('email').value = '';
-  document.getElementById('fullName').value = '';
+	document.getElementById("email").value = "";
+	document.getElementById("fullName").value = "";
 }
 
 function sendAlert(name) {
-  // eslint-disable-next-line no-alert
-  window.alert(`Thanks ${name}, We'll be in touch shortly!`);
+	// eslint-disable-next-line no-alert
+	window.alert(`Thanks ${name}, We'll be in touch shortly!`);
 }
 
 // eslint-disable-next-line no-unused-vars
 function submitMessage(event) {
-  event.preventDefault();
-  const fullName = document.getElementById('fullName').value;
+	event.preventDefault();
+	const fullName = document.getElementById("fullName").value;
 
-  sendAlert(fullName);
-  clearForm();
+	sendAlert(fullName);
+	clearForm();
 }
